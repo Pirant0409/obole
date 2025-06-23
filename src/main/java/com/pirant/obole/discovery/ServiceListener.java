928bb8e6964e61cf08026359c6ac96c49380aaf1
pirant0409 <ant.mau.pir@gmail.com>
2025-06-23 10:34:30 +0200

    Service Listener

--- src/main/java/com/pirant/obole/discovery/ServiceListener.java
+++ src/main/java/com/pirant/obole/discovery/ServiceListener.java
@@ -2,8 +2,23 @@ package com.pirant.obole.discovery;
 
 import javax.jmdns.ServiceEvent;
 import javax.jmdns.ServiceInfo;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class ServiceListener implements javax.jmdns.ServiceListener{
+
+    private final String localHostAddress;
+
+    public ServiceListener(){
+        String localAddress = "127.0.0.1";
+        try{
+            localAddress = InetAddress.getLocalHost().getHostAddress();
+        }
+        catch (UnknownHostException e){
+            System.err.println("Could not get local host address " + e.getMessage());
+        }
+        this.localHostAddress = localAddress;
+    }
     @Override
     public void serviceAdded(ServiceEvent event) {
         System.out.println("Service added: " + event.getName());
@@ -18,6 +33,11 @@ public class ServiceListener implements javax.jmdns.ServiceListener{
     @Override
     public void serviceResolved(ServiceEvent event) {
         ServiceInfo info = event.getInfo();
-        System.out.println("Service resolved: " + info.getName());
+
+        String remoteAddress = info.getHostAddresses()[0];
+
+        if (!remoteAddress.equals(localHostAddress)){
+            System.out.println("Service resolved: " + info.getName());
+        }
     }
 }
